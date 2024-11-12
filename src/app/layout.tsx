f7baf533eee68bd1b3ef0f4ce6fb577211adccb5
DavidBK999 <david.koeberl@gmx.at>
2024-11-12 14:36:01 +0100

    Metatags verbessert

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,12 @@ export const metadata: Metadata = {
   title: "David Köberl | Full Stack Web Developer",
   description:
     "Ich bin David Köberl, ein Full Stack Web Developer, spezialisiert auf die Entwicklung moderner und skalierbarer Webanwendungen. Entdecke meine Arbeit und Projekte auf dieser Portfolio-Seite.",
+  keywords:
+    "Webentwickler Wien, Full Stack Webentwickler, Webentwicklung Wien, professionelle Webentwicklung, Webentwicklung Wien Österreich, Webdesign Wien, React Entwickler Wien",
+  authors: [
+    { name: "David Köberl", url: "https://www.davidkoeberl.com" },
+    { name: "Frontend Tribe", url: "https://www.frontendtribe.com" },
+  ],
 };
 
 export default function RootLayout({
@@ -26,16 +32,59 @@ export default function RootLayout({
       <head>
         <link rel="canonical" href="https://www.davidkoeberl.com" />
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+
+        {/* Meta-Tags */}
+        <meta
+          name="description"
+          content="Willkommen auf der Portfolio-Seite von David Köberl. Ich bin ein Full Stack Webentwickler mit Erfahrung in Frontend und Backend Entwicklung."
+        />
+        <meta
+          name="keywords"
+          content="Webentwickler Wien, Full Stack Webentwickler, Webentwicklung Wien, professionelle Webentwicklung, Webentwicklung Wien Österreich"
+        />
+
+        {/* Open Graph Meta-Tags */}
+        <meta
+          property="og:title"
+          content="David Köberl - Full Stack Webentwickler"
+        />
+        <meta
+          property="og:description"
+          content="Willkommen auf der Portfolio-Seite von David Köberl. Ich bin ein Full Stack Web Entwickler mit Erfahrung in Frontend und Backend Entwicklung."
+        />
+        <meta
+          property="og:image"
+          content="https://davidkoeberl.com/favicon.ico"
+        />
+        <meta property="og:url" content="https://davidkoeberl.com" />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter Meta-Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="David Köberl | Full Stack Webentwickler"
+        />
+        <meta
+          name="twitter:description"
+          content="Willkommen auf der Portfolio-Seite von David Köberl. Ich bin ein Full Stack Web Entwickler mit Erfahrung in Frontend und Backend Entwicklung."
+        />
+        <meta
+          name="twitter:image"
+          content="https://davidkoeberl.com/favicon.ico"
+        />
+
+        {/* JSON-LD for Structured Data */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: JSON.stringify({
               "@context": "http://schema.org",
-              "@type": "Product",
-              name: "David Köberl Web Development Services",
+              "@type": "Person",
+              name: "David Köberl",
               image: "https://davidkoeberl.com/favicon.ico",
-              description: "Web Development Service von David Köberl",
-              brand: "David Köberl",
+              description: "Webentwicklung Services David Köberl",
+              url: "https://www.davidkoeberl.com",
               aggregateRating: {
                 "@type": "AggregateRating",
                 ratingValue: "5",
@@ -44,10 +93,7 @@ export default function RootLayout({
               review: [
                 {
                   "@type": "Review",
-                  author: {
-                    "@type": "Person",
-                    name: "Tamino Tschol",
-                  },
+                  author: { "@type": "Person", name: "Tamino Tschol" },
                   datePublished: "2024-11-07",
                   reviewBody:
                     "David ist ein hervorragender Entwickler, der unsere Anforderungen schnell und professionell umgesetzt hat.",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,56 +6,10 @@ import { TestimonialsSection } from "@/sections/Testimonials";
 import { AboutSection } from "@/sections/About";
 import { ContactSection } from "@/sections/Contact";
 import { Footer } from "@/sections/Footer";
-import Head from "next/head";
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>David Köberl - Full Stack Web Developer</title>
-        <meta
-          name="description"
-          content="Willkommen auf der Portfolio-Seite von David Köberl. Ich bin ein Full Stack Web Developer mit Erfahrung in Frontend und Backend Entwicklung."
-        />
-        <meta
-          name="keywords"
-          content="David Köberl, Portfolio, Full Stack Developer, Web Development, React, Node.js, Next.js"
-        />
-        <meta name="author" content="David Köberl" />
-
-        {/* Open Graph Meta-Tags */}
-        <meta
-          property="og:title"
-          content="David Köberl - Full Stack Web Developer"
-        />
-        <meta
-          property="og:description"
-          content="Willkommen auf der Portfolio-Seite von David Köberl. Ich bin ein Full Stack Web Developer mit Erfahrung in Frontend und Backend Entwicklung."
-        />
-        {/* Nutze ein passendes Bild, das mindestens 1200x630 px groß ist */}
-        <meta
-          property="og:image"
-          content="https://davidkoeberl.com/assets/images/portfolio-banner.jpg"
-        />
-        <meta property="og:url" content="https://davidkoeberl.com" />
-        <meta property="og:type" content="website" />
-
-        {/* Twitter Card Meta-Tags */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          name="twitter:title"
-          content="David Köberl - Full Stack Web Developer Portfolio"
-        />
-        <meta
-          name="twitter:description"
-          content="Willkommen auf der Portfolio-Seite von David Köberl. Ich bin ein Full Stack Web Developer mit Erfahrung in Frontend und Backend Entwicklung."
-        />
-        {/* Nutze hier das gleiche Bild wie bei Open Graph */}
-        <meta
-          name="twitter:image"
-          content="https://davidkoeberl.com/assets/images/portfolio-banner.jpg"
-        />
-      </Head>
       <div>
         <Header />
         <div id="Hero">
